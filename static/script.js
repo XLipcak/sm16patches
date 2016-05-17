c049b036a501f9516f38bf089f06b713901ce393
Richard Trebichavský <richard.trebichavsky@gmail.com>
2016-05-17 15:13:07 +0200

    Vue.js rewritten to JQuery

--- static/script.js
+++ static/script.js
@@ -1,26 +1,49 @@
-var EditableText = Vue.extend({
-	data: ['updatedPredicate', 'updatedData'],
-	props: ['predicate', 'data'],
-	// template: '<input value="{{ data }}">'
-})
-
-new Vue({
-	el: '.container',
-	data: {
-		editMode: true
-	},
-	components: {
-		'editable-text': EditableText
-	},
-	methods: {
-		toggleEditMode: function () {
-			this.editMode = !this.editMode;
-		},
-		generatePatch: function () {
-			console.log(_.map(this.$children, function(child) { return child.predicate + " -> " + child.data }));
-		}
-	}
-});
-
-console.log('Vue loaded');
+$(document).ready(function() {
+	var data = [];
+	var editMode = false;
 	
+	function findNonRecursive(element, selector, found) {
+		var newFound = found;
+		$(element).children().each(function (idx, child) {
+			if ($(child).is(selector)) {
+				newFound.push(child);
+			} else {
+				findNonRecursive(child, selector, newFound);
+			}
+		});
+		
+		return newFound;
+	}
+
+	function parseGroup(group) {
+		var editElement = $('.rdf-element:first', group);
+		
+		return {
+			name: editElement.attr('data-name'),
+			value: editElement.text(),
+			children: _.map(findNonRecursive(group, '.rdf-group', []), parseGroup)
+		};
+	};
+	
+	$('.button.rdf-toggle-edit-mode').click(function() {
+		editMode = !editMode;
+		$('.rdf-element').each(function (idx, el) {
+			$(el).attr('contenteditable', editMode);
+			if (editMode) {
+				$(el).addClass('rdf-element-in-edit');
+			} else {
+				$(el).removeClass('rdf-element-in-edit');
+			}
+		});
+	});
+	
+	$('.button.rdf-generate-patch').click(function() {
+		console.log(
+			JSON.stringify(
+				_.map(findNonRecursive($('.container'), '.rdf-group', []), parseGroup),
+				null,
+				2
+			)
+		)
+	});	
+});
